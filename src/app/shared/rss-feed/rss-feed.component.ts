d1d261fc73f6ec8be136da5a259292dc2bc4b2ae
inuwa <inuwa@hotmail.co.uk>
2020-08-23 18:29:47 +0100

    image error processing refactor

--- src/app/shared/rss-feed/rss-feed.component.ts
+++ src/app/shared/rss-feed/rss-feed.component.ts
@@ -1,8 +1,6 @@
-import { Component, OnInit, Input, ChangeDetectionStrategy, ChangeDetectorRef, OnDestroy } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectionStrategy, OnDestroy } from '@angular/core';
 import { RSS, Item } from 'src/app/_';
-import { HttpClient } from '@angular/common/http';
 import { Subscription, of } from 'rxjs';
-import { catchError } from 'rxjs/operators';
 
 @Component({
     selector: 'rss-feed',
@@ -10,24 +8,18 @@ import { catchError } from 'rxjs/operators';
     styleUrls: ['./rss-feed.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class RssFeedComponent implements OnInit, OnDestroy {
+export class RssFeedComponent implements OnInit {
     @Input() rssFeed: RSS;
     @Input() item: Item;
-    urlIsCorrect: boolean = false;
+    imageLoadingError: boolean = false;
     private _subcription: Subscription;
-    constructor(private _httpClient: HttpClient, private _changeDetectorRef: ChangeDetectorRef) { }
+    constructor() { }
 
     ngOnInit(): void {
         this._stripHTMLTags();
         if (this.item.content.length > 150) this.item.content = this._truncateString(this.item.content, 150);
-        this._urlIsCorrect()
     }
 
-    ngOnDestroy() {
-        if (!this._subcription) return;
-        this._subcription.unsubscribe();
-        this._subcription = null;
-    }
 
     private _stripHTMLTags() {
         this.item.content = this.item.content.replace(/(<([^>]+)>)/gi, '');
@@ -35,20 +27,6 @@ export class RssFeedComponent implements OnInit, OnDestroy {
 
     private _truncateString(truncatee: string, strLength: number) {
         const truncated = truncatee.substr(0, strLength);
-        return truncatee.substr(0, truncated.lastIndexOf(' ')) + '...';
-    }
-
-    private _urlIsCorrect() {
-        if (!this.item.enclosure.link) return this.urlIsCorrect;
-        this._subcription = this._httpClient.get(this.item.enclosure.link)
-            .pipe(
-                catchError((error) => {
-                    return (error.status !== 200) ? of(false) : of(true);
-                })
-            ).subscribe(e => {
-                this.urlIsCorrect = (e) ? true : false;
-                this._changeDetectorRef.detectChanges();
-            })
-
+        return truncatee.substr(0, truncated.lastIndexOf(' ')) + ' ...';
     }
 }
